8cdfd7d4c634cc0eea017bd912fddb257aa9d570
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-06-14 22:30:24 +0100

    update 404 styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useTheme } from "emotion-theming";
 
 interface FourOhFourProps {}
 
 const FourOhFour: React.FC<FourOhFourProps> = ({}) => {
-  return <span>Not found!</span>;
+  const theme = useTheme();
+
+  return (
+    <div
+      css={{
+        alignItems: "center",
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+      }}
+    >
+      <h4
+        css={{
+          borderRight: `1px solid ${theme.colors.surface}`,
+          fontSize: theme.fonts.size.dek,
+          fontWeight: 500,
+          padding: "10px 24px 10px 0",
+          marginRight: 24,
+        }}
+      >
+        404
+      </h4>
+      <span>This page could not be found.</span>
+    </div>
+  );
 };
 
 export default FourOhFour;
